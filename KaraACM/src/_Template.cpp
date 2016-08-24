6847c098f0a10f639c61835c51261409be9ba567
wudx <wudx@Ctrip.com>
2016-08-24 16:43:18 +0800

    template

--- KaraACM/src/_Template.cpp
+++ KaraACM/src/_Template.cpp
@@ -1,10 +1,14 @@
+#if 0
 #include <iostream>//cin//cout
 #include <cstdio>//stdin
 using namespace std;
 const char* IN_FILE = "C:/in.txt";
+#define DEBUG_ON1
 void wdx() {
 	std::ios_base::sync_with_stdio(false);
+#ifdef DEBUG_ON
 	freopen(IN_FILE, "r", stdin);
+#endif
 }
 int main() {
 	wdx();
@@ -14,3 +18,4 @@ int main() {
 	}
 	return 0;
 }
+#endif
